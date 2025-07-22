b9425465640bc3b713c88d136657ede4ac5f40fd
Farooq Hussain <farooqhussain9393@gmail.com>
2025-07-23 04:24:10 +0500

    Update settings.py

--- customer_churn_django/settings.py
+++ customer_churn_django/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'unsafe-default-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 
 # Application definition
